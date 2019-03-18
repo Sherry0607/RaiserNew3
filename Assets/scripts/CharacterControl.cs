4404dca4cdebbc4551b6bb7d1879df872d2c064c
qiuhuiting <595525684@qq.com>
2019-03-18 23:18:49 +0800

    加了电梯，直接复制场景里的就能用。整理了一下文件夹

--- Assets/scripts/CharacterControl.cs
+++ Assets/scripts/CharacterControl.cs
@@ -11,25 +11,20 @@ public class CharacterControl : MonoBehaviour
     public float JumpForce1 = 800;
     public float JumpForce2 = 500;
     public float MoveSpeed = 20;
-    private Rigidbody2D m_rigid;
+    public Rigidbody2D m_rigid;
     //获取 animator组件
     private Animator m_animator;
     private float horizontal = 0;
-    private float move = 0;
+    public float move = 0;
     bool isJump = false;
     bool isDoubleJump = false;
-
-    bool attack = false;
     public bool isAttacking;
-
+    public bool Movement = true; //主角是否可以移动
 
     public int life;
     public List<GameObject> lifeImg;
 
-
     public GameObject bos1, bos2;
-
-
 
     // Use this for initialization
     void Start()
@@ -66,89 +61,83 @@ public class CharacterControl : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Movement == true)
         {
-            // m_rigid.AddForce(new Vector2(0,JumpForce));
-            if (!isJump)//如果还在跳跃中，则不重复执行 
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                m_rigid.AddForce(new Vector2(0, JumpForce1));
-                isJump = true;
-                m_animator.SetBool("Jump", true);
-
-            }
-            else
-            {
-                m_animator.SetBool("Jump", false);
-
-                if (isDoubleJump)//判断是否在二段跳  
+                // m_rigid.AddForce(new Vector2(0,JumpForce));
+                if (!isJump)//如果还在跳跃中，则不重复执行 
                 {
-                    m_animator.SetBool("Jump", false);
-                    return;//否则不能二段跳  
+                    m_rigid.AddForce(new Vector2(0, JumpForce1));
+                    isJump = true;
+                    m_animator.SetBool("Jump", true);
 
                 }
                 else
                 {
-                    isDoubleJump = true;
-                    m_rigid.AddForce(new Vector2(0, JumpForce2));
-                    m_animator.SetBool("Jump", true);
+                    m_animator.SetBool("Jump", false);
 
+                    if (isDoubleJump)//判断是否在二段跳  
+                    {
+                        m_animator.SetBool("Jump", false);
+                        return;//否则不能二段跳  
+
+                    }
+                    else
+                    {
+                        isDoubleJump = true;
+                        m_rigid.AddForce(new Vector2(0, JumpForce2));
+                        m_animator.SetBool("Jump", true);
+
+                    }
+                }
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Debug.Log("attack");
+                m_animator.SetBool("attack", true);
+                isAttacking = true;
+                if (Vector2.Distance(transform.position, bos1.transform.position) <= 2.3f || Vector2.Distance(transform.position, bos2.transform.position) <= 2.3f)
+                {
+                    BossAI.instance.LifeChange();
                 }
             }
-
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Debug.Log("attack");
-            m_animator.SetBool("attack", true);
-            isAttacking = true;
-            //print(Vector2.Distance(transform.position, bos1.transform.position) + "_______" + Vector2.Distance(transform.position, bos2.transform.position));
-            if (Vector2.Distance(transform.position,bos1.transform.position)<=2.3f|| Vector2.Distance(transform.position, bos2.transform.position)<=2.3f)
+            else
             {
-                BossAI.instance.LifeChange();
-            }
-        }
-        else
-        {
-            m_animator.SetBool("attack", false);
-            isAttacking = false;
+                m_animator.SetBool("attack", false);
+                isAttacking = false;
 
-        }
+            }
 
 
             horizontal = Input.GetAxis("Horizontal");
-        move = horizontal * MoveSpeed;
+            move = horizontal * MoveSpeed;
 
-        //
-        if (move > 0)
-        {
-          this.transform.localScale=new Vector3( Mathf.Abs(this.transform.localScale.x),
-                                                this.transform.localScale.y,
-                                                this.transform.localScale.z);
+            if (move > 0)
+            {
+                this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x),
+                                                      this.transform.localScale.y,
+                                                      this.transform.localScale.z);
+            }
+
+
+            if (move < 0)
+            {
+                this.transform.localScale = new Vector3(-Mathf.Abs(this.transform.localScale.x),
+                                                      this.transform.localScale.y,
+                                                      this.transform.localScale.z);
+            }
+
+            m_rigid.velocity = new Vector2(move, m_rigid.velocity.y);
         }
-
-
-        if (move < 0)
-        {
-            this.transform.localScale = new Vector3(-Mathf.Abs(this.transform.localScale.x),
-                                                  this.transform.localScale.y,
-                                                  this.transform.localScale.z);
-        }
-
-        m_rigid.velocity = new Vector2(move, m_rigid.velocity.y);
 
         //取移动速度的绝对值，>0.1  播放move动画    <0.1 播放Idle动画 
         m_animator.SetFloat("move", Mathf.Abs(move));
 
     }
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(1);//运行到这，暂停t秒
-        isJump = false;
-       
 
-    }
     public void LifeChange(bool aa)
     {
         if (aa&&life<6)
---
+++ Assets/scripts/Elevator.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    public float waitingTime; //电梯上的等待时间
+    public GameObject rod;
+    public float RodSpeed; //杆的伸长速度
+    public float ElevatorSpeed;
+
+    private GameObject player;
+    float staySecond; //玩家在电梯上停留的时间
+    bool moveUp; //电梯可否向上移动
+    bool atTop; //电梯是否在顶端
+    float length;
+    float PosY;
+
+    // Use this for initialization
+    void Start () {
+        atTop = true;
+        player = GameObject.FindGameObjectWithTag("Player");
+        PosY = transform.position.y;
+        length = rod.transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (staySecond >= waitingTime && moveUp == true && atTop == false) //停留一定时间后电梯开始向上运动
+        {
+            length -= Time.deltaTime * RodSpeed;
+            PosY += Time.deltaTime * ElevatorSpeed;
+
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation; //冻结旋转
+            rod.transform.localScale = new Vector3(1, length, 1);
+            transform.position = new Vector3(transform.position.x, PosY, transform.position.z);
+
+        }
+
+        if (staySecond >= waitingTime && moveUp == false  && atTop == true) //向下
+        {
+            length += Time.deltaTime * RodSpeed;
+            PosY -= Time.deltaTime * ElevatorSpeed;
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            rod.transform.localScale = new Vector3(1, length, 1);
+            transform.position = new Vector3(transform.position.x, PosY, transform.position.z);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            staySecond += Time.deltaTime; //计算玩家停留在电梯上的时间
+            if (moveUp == false && atTop == true)
+            {
+                player.transform.parent = gameObject.transform;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "电梯顶端") //电梯到达终点位置
+        {
+            player.GetComponent<CharacterControl>().Movement = true;
+            player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            Invoke("StopMovement", 0.1f);
+            moveUp = false;
+            player.GetComponent<Rigidbody2D>().gravityScale = 4;
+        }
+
+        if (collision.name == "电梯底端") //电梯到达起始位置
+        {
+            player.GetComponent<CharacterControl>().Movement = true;
+            transform.DetachChildren();
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            moveUp = true;
+            player.GetComponent<Rigidbody2D>().gravityScale = 4;
+        }
+
+        if (collision.tag == "Player")
+        {
+            player.GetComponent<CharacterControl>().Movement = false;
+            player.GetComponent<CharacterControl>().move = 0;
+            player.GetComponent<CharacterControl>().m_rigid.velocity = new Vector2(0, 0);
+            player.GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            length = rod.transform.localScale.y;
+
+            staySecond = 0;
+            if (moveUp == false)
+            {
+                atTop = true;
+            }
+            else
+            {
+                atTop = false;
+            }
+        }
+    }
+
+    void StopMovement() //抵消上升惯性
+    {
+        player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+    }
+
+}
