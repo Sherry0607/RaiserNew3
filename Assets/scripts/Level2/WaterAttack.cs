9d1bc27e8cd8d5de9e56ca00258adbe3a65c6ab3
我是一只大笨熊 <595717967@qq.com>
2019-04-25 12:29:17 +0800

    毒池掉血

---
+++ Assets/scripts/Level2/WaterAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaterAttack : MonoBehaviour {
+
+    private CharacterControl2 playerCtr;
+    private Transform Player;
+    Animator Animators;
+   
+
+    // Use this for initialization
+    void Start () {
+        playerCtr = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterControl2>();
+        Animators = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag.Contains("Player"))
+        {
+                                      
+                
+            Animators.SetBool("hurt", true);
+            playerCtr.LifeChange(false);
+            Invoke("ResetTransform", 1.0f);
+                          
+                     
+        }
+       
+  }
+
+    private void ResetTransform()
+    {
+        Player.transform.position = new Vector3(-16.52f, -14.5f, 0);
+    }
+
+}
+
