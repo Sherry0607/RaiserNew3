91ca04359ac4d34bb9acad007bb6089589a0bf9e
我是一只大笨熊 <595717967@qq.com>
2019-04-26 15:43:02 +0800

    毒池1秒掉一滴血，毒池平台位置下移

--- Assets/scripts/Level2/WaterAttack.cs
+++ Assets/scripts/Level2/WaterAttack.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public class WaterAttack : MonoBehaviour {
+
+    public float attackTime = 1f;
+    private float attackTimer;
 
     private CharacterControl2 playerCtr;
     private Transform Player;
@@ -12,35 +13,35 @@ public class WaterAttack : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+
         playerCtr = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterControl2>();
         Animators = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
         Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        attackTimer = attackTime;
     }
 	
-	// Update is called once per frame
-	void Update () {
-        
-    }
 
-    void OnTriggerEnter2D(Collider2D col)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-
-        if (col.tag.Contains("Player"))
+        if (collision.tag.Contains("Player"))
         {
-                                      
-                
-            Animators.SetBool("hurt", true);
-            playerCtr.LifeChange(false);
-            Invoke("ResetTransform", 1.0f);
-                          
-                     
+            attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0) {
+                attackTimer = attackTime;
+                ResetTransform();
+
+            }
         }
-       
-  }
+
+    }
 
     private void ResetTransform()
     {
-        Player.transform.position = new Vector3(-16.52f, -14.5f, 0);
+        Animators.SetBool("hurt", true);
+        playerCtr.LifeChange(false);
+       // Player.transform.position = new Vector3(-16.52f, -14.5f, 0);
     }
 
 }
