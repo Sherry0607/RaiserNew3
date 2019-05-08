614208d8e9bc197985db46508289bcb5099fcd9f
LiangYudi <751526618@qq.com>
2019-05-09 07:52:34 +0800

    调了lv2刺刺和大电梯的触发和动画 安全屋前景

---
+++ Assets/scripts/Level2/cici.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Anima2D; //加上命名空间
+
+public class cici : MonoBehaviour {
+    private Animator m_animator;
+    public bool ispushing = false;
+   
+    // Use this for initialization
+    void Start () {
+        m_animator = GetComponent<Animator>();
+        // ispushing = false;
+        m_animator.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            // ispushing = true;
+            m_animator.enabled = true;
+           
+        }
+    }
+    
+
+}
