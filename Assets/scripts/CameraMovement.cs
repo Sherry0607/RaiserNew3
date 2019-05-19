7cdc1f2cf1e23e74f7227374311db5dac1c7ac2d
qiuhuiting <595525684@qq.com>
2019-05-19 17:08:49 +0800

    我觉得不能出问题 你呢

--- Assets/scripts/CameraMovement.cs
+++ Assets/scripts/CameraMovement.cs
@@ -9,42 +9,84 @@ public class CameraMovement : MonoBehaviour {
     public float UpSpeed;
     public float MaxDis;
 
-    float UpDis;
+    public float UpDis;
     GameObject Player;
     CinemachineVirtualCamera m_Cinemachine;
+    bool isFollow;
+    bool isFollow2;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Player = GameObject.FindGameObjectWithTag("Player");
         m_Cinemachine = GetComponent<CinemachineVirtualCamera>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.W)) //镜头上移
         {
             m_Cinemachine.m_Follow = null;
+
             if (UpDis < MaxDis)
             {
                 UpDis += Time.deltaTime * UpSpeed;
-                transform.position = new Vector3(transform.position.x, transform.position.y + UpDis, transform.position.z);
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y + UpDis, transform.position.z);
             }
+            else
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y, transform.position.z);
         }
 
         if (Input.GetKey(KeyCode.S)) //下移
         {
             m_Cinemachine.m_Follow = null;
-            if (Mathf.Abs( UpDis) < MaxDis)
+            if (Mathf.Abs(UpDis) < MaxDis)
             {
                 UpDis -= Time.deltaTime * UpSpeed;
-                transform.position = new Vector3(transform.position.x, transform.position.y + UpDis, transform.position.z);
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y + UpDis, transform.position.z);
+            }
+            else
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isFollow = true;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            isFollow2 = true;
+        }
+
+        if (isFollow)
+        {
+            if (UpDis > 0)
+            {
+                UpDis -= Time.deltaTime * UpSpeed;
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y - UpDis, transform.position.z);
+            }
+            else
+            {
+                m_Cinemachine.m_Follow = Player.transform;
+                UpDis = 0;
+                isFollow = false;
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        if (isFollow2)
         {
-            m_Cinemachine.m_Follow = Player.transform;
-            UpDis = 0;
+            if (UpDis < 0)
+            {
+                UpDis += Time.deltaTime * UpSpeed;
+                transform.position = new Vector3(Player.transform.position.x, transform.position.y + Mathf.Abs(UpDis), transform.position.z);
+            }
+            else
+            {
+                m_Cinemachine.m_Follow = Player.transform;
+                UpDis = 0;
+                isFollow2 = false;
+            }
         }
-	}
+    }
 }
